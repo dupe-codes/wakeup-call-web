c38ed98e367cea06df26f69672069ae984da24cc
njdup <njdupp@gmail.com>
2015-01-10 23:41:44 -0800

    fixed issue with development settings

--- utils/settings.py
+++ utils/settings.py
@@ -4,7 +4,7 @@ Settings specific to the functionality of app utilities
 
 import os
 
-if os.environ.get('MODE') == 'DEV':
-    BACKEND_URL = 'http://localhost:8080'
-else:
+if os.environ.get('MODE') == 'PROD':
     BACKEND_URL = 'https://wakeup-backend.herokuapp.com'
+else:
+    BACKEND_URL = 'http://localhost:8080'
